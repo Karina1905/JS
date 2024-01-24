046ffb3e341d8239300b6bd1349f78220d683361
Karina1905 <martarina1905@gmail.com>
2024-01-24 13:37:11 +0200

    add text

--- HW/hw-1/homework-1.js
+++ HW/hw-1/homework-1.js
@@ -7,3 +7,9 @@ let d = 9;
 counter = counter + 4;
 const sum = a + b + c + d;
 console.log(sum/counter)
+
+const part1 = "F34";
+const part2 = "is";
+const part3 = "the best!"
+ 
+console.log(part1 + part2 + part3)
