0ca6c0176963183a7abdc251d99b2eb62fb76f78
Karina1905 <152206965+Karina1905@users.noreply.github.com>
2024-04-22 23:38:00 +0300

    hw-2

--- DOM/events/main.js
+++ DOM/events/main.js
@@ -1,16 +1,16 @@
 // DOM
 
 const scripts = document.scripts;
-console.log(scripts, scripts.length);
+console.log(scripts, "scripts", scripts.length);
 
 const myOwnCustomScript = document.createElement("script");
 myOwnCustomScript.src = "./hello.js";
 document.body.appendChild(myOwnCustomScript);
 
-console.log(scripts, myOwnCustomScript.src);
+console.log(scripts, "scripts", myOwnCustomScript.src);
 
 const forms = document.forms;
-console.log(forms);
+console.log(forms, "forms");
 
 for (const form of forms) {
   form.onsubmit = (event) => {
@@ -29,59 +29,66 @@ const images = [
 // .src =
 
 images.forEach((src) => {
-  const img = document.createElement("img");
-  img.src = src;
-
-  document.body.appendChild(img);
+  const image = document.createElement("img");
+  image.src = src;
+  document.body.appendChild(image);
 });
-console.log(document.images);
+
+console.log(document.images, "images");
 
 const styles = document.styleSheets;
-console.log(styles);
+console.log(styles, "styles");
 
-styles.title = "TITLE FROM JS";
-console.log(document.head);
+document.title = "TITLE FROM JS";
+console.log(document.head, "head");
 
 let count = 0;
 
 setInterval(() => {
-  count += 1;
+  count = count + 1;
   document.title = count;
 }, 1000);
 
 const style = document.createElement("style");
 style.innerHTML = `
 body {
-    background: red;
+    background: lightblue;
 }`;
 
 document.head.appendChild(style);
 
-// Events in DOM
+// EVENTS in DOM
+
 let count2 = 0;
+
 const count2Element = document.createElement("h1");
 count2Element.textContent = count2;
 document.body.appendChild(count2Element);
 
-const button = document.createElement("button");
-button.textContent = "+1";
+const resetCount2 = () => {
+  count2 = 0;
+  count2Element.textContent = count2;
+};
 
-button.onclick = () => {
+// +1
+const increaseButton = document.createElement("button");
+increaseButton.textContent = "+1";
+increaseButton.onclick = () => {
   count2 = count2 + 1;
   count2Element.textContent = count2;
-  console.log("The button was clicked", count2);
 };
+increaseButton.ondblclick = resetCount2;
+document.body.appendChild(increaseButton);
 
-document.body.appendChild(button);
-
-const button1 = document.createElement("button");
-button1.textContent = "-1";
-
-button1.onclick = () => {
-  count2 -= 1;
+// -1
+const decreaseButton = document.createElement("button");
+decreaseButton.textContent = "-1";
+decreaseButton.onclick = () => {
+  count2 = count2 - 1;
   count2Element.textContent = count2;
 };
-document.body.appendChild(button1);
+decreaseButton.ondblclick = resetCount2;
+document.body.appendChild(decreaseButton);
 
 // Main events:
 // onclick, ondbllclick, onChange, onInput, onMouseMove
@@ -89,38 +96,49 @@ document.body.appendChild(button1);
 
 const dblClickButton = document.createElement("button");
 dblClickButton.textContent = "Click 2 times";
+
 dblClickButton.ondblclick = function () {
   console.log("DBL click was fired");
 };
+
 dblClickButton.onclick = function (event) {
-  console.log(event);
+  console.log(event, "event");
   console.log("Click was fired");
 };
+
 document.body.appendChild(dblClickButton);
+
 // Завдання:
 // Використовуючи dblClick на 2 попередніх кнопках
 // реалізувати логіку обнулення count2 (має виводитись)
-dblClickButton.ondblclick = () => {
-  count2 = 0;
-  count2Element.textContent = count2;
-  console.log("count2 was reset", count2);
-};
-const selectButtonWrapper = document.getElementById("select-buttons-wrapper");
+
+const selectButtonsWrapper = document.getElementById("select-buttons-wrapper");
 
 const selectButtonsNumbers = [1, 2, 3];
+let selectedButtons = [];
+
 const selectButtons = selectButtonsNumbers.map((number) => {
   const selectBtn = document.createElement("button");
   selectBtn.textContent = number;
 
-  const dblClickButton = document.createElement("button");
-  dblClickButton.textContent = "Click 2 times";
-  dblClickButton.ondblclick = function () {
-    console.log("DBL click was fired");
-  };
-  dblClickButton.onclick = function () {
-    console.log("Click was fired");
-  };
-  document.body.appendChild(dblClickButton);
+  selectBtn.onclick = (event) => {
+    if (event.ctrlKey && !selectButtons.includes(number)) {
+      selectedButtons.push(number);
+      event.target.classList.add("selected");
 
-  selectButtonWrapper.appendChild(selectBtn);
+      console.log(event.target);
+
+      console.log(selectedButtons, "selectedButtons");
+      
+    } else if (event.shiftKey && selectButtons.includes(number)) {
+      selectedButtons = selectedButtons.filter((n) => n !== number)
+      event.target.classList.remove("selected");
+      console.log(event.shiftKey, "selectedButtons");
+    }
+    console.log(number);
+  };
+
+  selectButtonsWrapper.appendChild(selectBtn);
+
 });
+
