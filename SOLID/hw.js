0f8bb5c971fc43e3b526ea6a4826560809a6d620
Karina1905 <152206965+Karina1905@users.noreply.github.com>
2024-07-06 20:54:50 +0300

    solid update

--- SOLID/hw.js
+++ SOLID/hw.js
@@ -1,36 +1,83 @@
-class Vehicle{
-  constructor(name, speed) {
-    this.name = name;
-    this.speed = speed;
+// L - Liskov Substitution Principe
+
+// before
+// class Vehicle {
+//     constructor(name, speed) {
+//       this.name = name;
+//       this.speed = speed;
+//     }
+  
+//     startEngine() {
+//       console.log(`${this.name} is starting engine`);
+//     }
+  
+//     move() {
+//       console.log(`${this.name} is moving at speed ${this.speed}`);
+//     }
+//   }
+  
+//   class Car extends Vehicle {
+//     drift() {
+//       console.log(`${this.name} is drifting at speed ${this.speed}`);
+//     }
+//   }
+  
+//   class Bicycle extends Vehicle {
+//     ring() {
+//       console.log(`${this.name} is ringing the bell`);
+//     }
+//   }
+
+  
+//   const car = new Car('Car', 100);
+//   car.startEngine(); 
+//   car.move();       
+//   car.drift();       
+  
+//   const bicycle = new Bicycle('Bicycle', 20);
+//   bicycle.startEngine(); 
+//   bicycle.move();      
+//   bicycle.ring();    
+
+
+  // after
+
+class Vehicle {
+    constructor(name, speed) {
+      this.name = name;
+      this.speed = speed;
+    }
   }
-}
-
-class VehicleWithoutEngine extends Vehicle {
+  
+  class VehicleWithoutEngine extends Vehicle {
     move() {
-        console.log("Moving device without engine");
-      }
-}
-
-class VehicleWithEngine extends Vehicle {
+      console.log(`${this.name} is moving at speed ${this.speed}`);
+    }
+  }
+  
+  class VehicleWithEngine extends Vehicle {
     startEngine() {
-        console.log("Starting engine of device");
-      }
-}
+      console.log(`${this.name} is starting engine`);
+    }
+  }
+  
+  class Car extends VehicleWithEngine {  
+    drift() {
+      console.log(`${this.name} is drifting at speed ${this.speed}`);
+    }
+  }
+  
+  class Bicycle extends VehicleWithoutEngine {
+    ring() {
+      console.log(`${this.name} is ringing the bell`);
+    }
+  }
+  
+  const car = new Car('Car', 100);
+  car.startEngine();          
+  car.drift();        
+  
+  const bicycle = new Bicycle('Bicycle', 20);
+  bicycle.move();        
+  bicycle.ring();    
 
-class Car extends VehicleWithEngine {
-    startEngine() {
-        console.log("Starting engine of car");
-      }
-}
-
-
-class Bicycle extends VehicleWithoutEngine {
-    move() {
-        console.log("Moving bicycle");
-      }
-}
-
-const bicycle = new Bicycle();
-bicycle.move();
-const car = new Car();
-car.startEngine();
